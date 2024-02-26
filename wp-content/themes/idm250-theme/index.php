0b06a42a6935c61779f60e47b078acbbed5233b3
elijor <ejg96@drexel.edu>
2024-02-26 00:03:25 -0500

    quick page fix and update imgs

--- wp-content/themes/idm250-theme/index.php
+++ wp-content/themes/idm250-theme/index.php
@@ -1,7 +1,14 @@
     <?php get_header(); ?>
-    <h1>
+    <!-- <h1>
+
+    </h1>  -->
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/CTWmcZqiy0Q?si=KkVRF9irKdZk8xRX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    
     <?php
-    echo get_the_title();
-    ?>
-    </h1> 
-    <?php get_footer(); ?>
+    echo get_the_content();
+    if (is_active_sidebar("main_sidebar")) { 
+        dynamic_sidebar("main_sidebar");
+    };
+
+    
+    get_footer(); ?>
